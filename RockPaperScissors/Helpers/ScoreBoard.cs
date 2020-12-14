81cbec1af147e9fd3f5be04863c104f6cd6e69ec
kipack <kipack.jeong@outlook.com>
2020-12-14 18:20:15 -0500

    Changed Image Added Scoreboard

---
+++ RockPaperScissors/Helpers/ScoreBoard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors.Helpers
+{
+    public class ScoreBoard
+    {
+        public int Score { get; set; } = 0;
+    }
+}
--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -20,6 +20,8 @@ namespace RockPaperScissors
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddTransient<ScoreBoard>();
             builder.Services.AddBootstrapCSS();
             builder.Services.AddTransient<GameMaterials>();
             await builder.Build().RunAsync();
