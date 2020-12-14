9305224e6e358e9ac54ef202e3a2a6f7cb58665b
kipack <kipack.jeong@outlook.com>
2020-12-14 14:47:06 -0500

    Created Components Connected with TM

--- RockPaperScissors/wwwroot/js/JavaScript.js
+++ RockPaperScissors/wwwroot/js/JavaScript.js
@@ -2,12 +2,12 @@
 // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
 
 // the link to your model provided by Teachable Machine export panel
-const URL = "https://teachablemachine.withgoogle.com/models/E50e4dex0/";
+export const URL = "https://teachablemachine.withgoogle.com/models/E50e4dex0/";
 
-let model, webcam, labelContainer, maxPredictions;
+export let model, webcam, labelContainer, maxPredictions;
 
 // Load the image model and setup the webcam
-async function init() {
+export function init() {
     const modelURL = URL + "model.json";
     const metadataURL = URL + "metadata.json";
 
