a8af8413cfedeb0a6f56d52bffe40e9d836a0458
kipack <kipack.jeong@outlook.com>
2020-12-13 19:32:15 -0500

    Add project files.

---
+++ RockPaperScissors/Helpers/GameMaterials.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+
+namespace RockPaperScissors.Helpers
+{
+    public class GameMaterials
+    {
+        public List<Hand> Hands = new List<Hand>()
+            {
+                new Hand
+                {
+                    Selection = OptionsRPS.Rock,
+                    LosesAgainst = OptionsRPS.Paper,
+                    WinAgainst = OptionsRPS.Scissors,
+                    Image = "rock.jpg"
+                },
+                new Hand
+                {
+                    Selection = OptionsRPS.Paper,
+                    LosesAgainst = OptionsRPS.Scissors,
+                    WinAgainst = OptionsRPS.Rock,
+                    Image = "paper.jpg"
+                },
+                new Hand
+                {
+                    Selection = OptionsRPS.Scissors,
+                    LosesAgainst = OptionsRPS.Rock,
+                    WinAgainst = OptionsRPS.Paper,
+                    Image = "scissor.jpg"
+                }
+            };
+        public Timer Timer { get; set; } = new Timer();
+        public Hand OpponentHand { get; set; } = new Hand
+        {
+            Selection = OptionsRPS.Rock,
+            LosesAgainst = OptionsRPS.Paper,
+            WinAgainst = OptionsRPS.Scissors,
+            Image = "rock.jpg"
+        };
+        public bool Lost { get; set; } = false;
+    }
+}
---
+++ RockPaperScissors/Helpers/Hand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors.Helpers
+{
+    public class Hand
+    {
+        public OptionsRPS Selection { get; set; }
+        public OptionsRPS WinAgainst { get; set; }
+        public OptionsRPS LosesAgainst { get; set; }
+        public string Image { get; set; }
+
+        public GameStatus PlayAgainst(Hand opponentHand)
+        {
+            if (Selection == opponentHand.Selection)
+            {
+                return GameStatus.Draw;
+            }
+            else if (LosesAgainst == opponentHand.Selection)
+            {
+                return GameStatus.Lost;
+            }
+
+            return GameStatus.Win;
+        }
+    }
+}
